b6fb861a33bf9770528b5c0ab6a1fceb2ec0a039
Vasyl Semak <vasylsmk1401@gmail.com>
2020-09-24 12:23:39 -0400

    implement add item to list

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
+const form = document.getElementById('addForm');
+const itemsList = document.getElementById('items');
 
+form.addEventListener('submit', addItem);
+
+// Add Item to List
+function addItem(e) {
+  e.preventDefault();
+  const input = document.getElementById('item').value;
+  const li = document.createElement('li');
+  li.className = 'list-group-item';
+
+  const deleteBtn = document.createElement('button');
+  deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
+  deleteBtn.appendChild(document.createTextNode('X'));
+
+  li.appendChild(document.createTextNode(input));
+  li.appendChild(deleteBtn);
+
+  itemsList.appendChild(li);
+}
